36e536297a60a1da4bc88429b64d0cebaa6026c2
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-12 21:31:46 +0530

    Checking Two Integers Are Equal or Not Equal.

--- ProgrammingConstruct/IfElseProblems.cs
+++ ProgrammingConstruct/IfElseProblems.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ProgrammingConstruct
 {
-    public class Problems
+    public class IfElseProblems
     {
         public void CheckIntegersEqualOrNot()
         {
--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            Problems problem = new Problems();
+            IfElseProblems problem = new IfElseProblems();
             problem.CheckIntegersEqualOrNot();
         }
     }
