4605f3683dabb4398d47a67b6a46e902730d8f2b
[BridgeLab3341] <[somashekarvs1996@gmail.com]>
2023-06-12 21:27:09 +0530

    Checking Two Integers Are Equal or Not Equal.

---
+++ ProgrammingConstruct/Problems.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProgrammingConstruct
+{
+    public class Problems
+    {
+        public void CheckIntegersEqualOrNot()
+        {
+            Console.WriteLine("Enter Two Numbers");
+            int num1=Convert.ToInt32(Console.ReadLine());
+            int num2=Convert.ToInt32(Console.ReadLine());
+            if(num1 == num2)
+            {
+                Console.WriteLine("{0} and {1} are Equal",num1,num2);
+            }
+            else
+            {
+                Console.WriteLine("{0} and {1} are NotEqual", num1, num2);
+            }
+        }
+    }
+}
---
+++ ProgrammingConstruct/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ProgrammingConstruct
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Problems problem = new Problems();
+            problem.CheckIntegersEqualOrNot();
+        }
+    }
+}
